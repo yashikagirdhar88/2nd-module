84a2884638b0fa2ad9dbdf51fd27ca0313ade9c3
yashikagirdhar88 <118722993+yashikagirdhar88@users.noreply.github.com>
2024-06-11 01:24:58 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,7 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
-
-//import "hardhat/console.sol";
 
 contract Assessment {
     address payable public owner;
@@ -22,20 +19,20 @@ contract Assessment {
     function deposit(uint256 _amount) public payable {
         uint _previousBalance = balance;
 
-        // make sure this is the owner
+       
         require(msg.sender == owner, "You are not the owner of this account");
 
-        // perform transaction
+       
         balance += _amount;
 
-        // assert transaction completed successfully
+        
         assert(balance == _previousBalance + _amount);
 
-        // emit the event
+      
         emit Deposit(_amount);
     }
 
-    // custom error
+   
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
@@ -48,13 +45,13 @@ contract Assessment {
             });
         }
 
-        // withdraw the given amount
+      
         balance -= _withdrawAmount;
 
-        // assert the balance is correct
+        
         assert(balance == (_previousBalance - _withdrawAmount));
 
-        // emit the event
+       
         emit Withdraw(_withdrawAmount);
     }
 }
